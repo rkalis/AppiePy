8e4cee25453d5798b7070eb4bc4d6bb83244b7f8
Rosco Kalis <roscokalis@gmail.com>
2018-03-24 13:50:50 +0100

    Added nutrition parsing

---
+++ appiepy/appiepy.py
@@ -0,0 +1,47 @@
+import requests as requests
+from urllib.parse import urlparse as urlparse
+from xml.etree import ElementTree as ElementTree
+
+def _get_request_url(product_url):
+    path = urlparse(product_url).path
+    request_url = 'https://www.ah.nl/service/rest/delegate?url={}'.format(path)
+    return request_url
+
+def _parse_nutrition(string):
+    string = string.split('[/table]')[0] + '[/table]'
+    string = string.replace('[', '<').replace(']', '>')
+    table = ElementTree.XML(string)[1:]
+    nutrition = {}
+    for row in table:
+        nutrition[row[0].text] = row[1].text
+    return nutrition
+
+class Product():
+    def __init__(self, url):
+        request_url = _get_request_url(url)
+        res = requests.get(request_url).json()
+        for lane in res['_embedded']['lanes']:
+            if lane['type'] == 'ProductDetailLane':
+                for item in lane['_embedded']['items']:
+                    if item['type'] != 'Product':
+                        continue
+                    product = item['_embedded']['product']
+                    self.id = product['id']
+                    self.brand = product['brandName']
+                    self.description = product['description']
+                    self.summary = product['details']['summary']
+                    self.unit_size = product['unitSize']
+                    self.category = product['categoryName']
+                    self.is_available = product['availability']['orderable']
+                    self.price = product['priceLabel']
+                    self.is_discounted = 'discount' in product
+            elif lane['type'] == 'StoryLane':
+                for item in lane['_embedded']['items']:
+                    for section in item['_embedded']['sections']:
+                        for content in section['_embedded']['content']:
+                            if 'features' in content:
+                                self.features = content['features']
+                            if 'text' in content and \
+                               'body' in content['text'] and \
+                               'Eiwitten' in content['text']['body']:
+                               self.nutrition = _parse_nutrition(content['text']['body'])
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,46 @@
 import appiepy
 import unittest
 
-class ApplesTestCase(unittest.TestCase):
-    def setUp(self):
-        self.product = appiepy.Product('https://www.ah.nl/producten/product/wi129993/ah-zoete-kleine-appeltjes')
+class ProductTests(unittest.TestCase):
+    def test_url_path(self):
+        # given
+        url = '/producten/product/wi395948/ah-kleintje-spinazie'
 
-    def test(self):
-        print(vars(self.product))
+        # when
+        product = appiepy.Product(url)
+
+        # then
+        self.assertIsInstance(product.id, str)
+        self.assertIsInstance(product.brand, str)
+        self.assertIsInstance(product.description, str)
+        self.assertIsInstance(product.summary, str)
+        self.assertIsInstance(product.unit_size, str)
+        self.assertIsInstance(product.category, str)
+        self.assertIsInstance(product.is_available, bool)
+        self.assertIsInstance(product.price, dict)
+        self.assertIsInstance(product.is_discounted, bool)
+        self.assertIsInstance(product.features, list)
+        self.assertIsInstance(product.nutrition, dict)
+
+    def test_full_url(self):
+        # given
+        url = 'https://www.ah.nl/producten/product/wi395948/ah-kleintje-spinazie'
+
+        # when
+        product = appiepy.Product(url)
+
+        # then
+        self.assertIsInstance(product.id, str)
+        self.assertIsInstance(product.brand, str)
+        self.assertIsInstance(product.description, str)
+        self.assertIsInstance(product.summary, str)
+        self.assertIsInstance(product.unit_size, str)
+        self.assertIsInstance(product.category, str)
+        self.assertIsInstance(product.is_available, bool)
+        self.assertIsInstance(product.price, dict)
+        self.assertIsInstance(product.is_discounted, bool)
+        self.assertIsInstance(product.features, list)
+        self.assertIsInstance(product.nutrition, dict)
 
 if __name__ == '__main__':
     unittest.main()
